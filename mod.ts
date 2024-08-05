c979e0dc879d3ef4ed5ed6b6fd554d21fcb637de
Luma <world@luma.email>
2024-08-06 02:48:36 +0900

    .

--- mod.ts
+++ mod.ts
@@ -1,1 +1,4 @@
-export const f = <T>() => <G>(x: T & G) => x;
+export const f1 = <T>() => <G>(x: T & G) => x;
+export type F1 = typeof f1;
+export const f2: F1 = () => (x) => x;
+export const f3: <T>() => <G>(x: T & G) => T & G = () => (x) => x;
